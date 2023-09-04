001161a092a5d1b3e20cb8446c6d0323a3d25911
ikbalhossainn <ikbalhossain125@gmail.com>
2023-09-04 13:25:44 +0600

    updated

--- Ikbal_Hossain_1277699/AJAX/5.1_student_submit.php
+++ Ikbal_Hossain_1277699/AJAX/5.1_student_submit.php
@@ -1,14 +1,14 @@
 <?php 
-    $db = new mysqli($host, $user, $password, $database);
+    $db = new mysqli("localhost", "root", "", "wdpf55_php");
     
     // print_r($_POST);
     extract($_POST);
 
-     echo "INSERT INTO students VALUES(NULL, '$name', '$gender', '$district')";
-    // $sql = ("INSERT INTO students VALUES(NULL, '$name', '$gender', '$district')");
-    // $db->query($sql);
+    //  echo "INSERT INTO students VALUES(NULL, '$name', '$gender', '$district')";
+    $sql = ("INSERT INTO students VALUES(NULL, '$name', '$gender', '$district')");
+    $db->query($sql);
 
-    // if($db->affected_rows>0){
-    //     echo "Inserted Successfully";
-    // }
+    if($db->affected_rows>0){
+        echo "Inserted Successfully";
+    }
 ?>
