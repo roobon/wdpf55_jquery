6ab1612db03557fde9bc23765144cb18d6ddd1d2
unknown <you@example.com>
2023-09-05 11:59:58 +0600

    add

---
+++ Ali_Ibne_Imran/jquery_ajax_crud/db_config.php
@@ -0,0 +1,7 @@
+<?php
+$server_name= "localhost";
+$user_name= "root";
+$password= "";
+$database_name= "wdpf55php_ajax";
+$conn= mysqli_connect($server_name , $user_name , $password , $database_name); 
+?>
