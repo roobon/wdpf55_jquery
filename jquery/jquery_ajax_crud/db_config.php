5cbb4667d5f2f416e451acf5ceee82d298709a64
ikbalhossainn <ikbalhossain125@gmail.com>
2023-09-05 09:43:58 +0600

    updated

--- jquery/jquery_ajax_crud/db_config.php
+++ jquery/jquery_ajax_crud/db_config.php
@@ -2,6 +2,6 @@
 $server_name= "localhost";
 $user_name= "root";
 $password= "";
-$database_name= "php_ajax";
+$database_name= "wdpf55php_ajax";
 $conn= mysqli_connect($server_name , $user_name , $password , $database_name); 
 ?>
