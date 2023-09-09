c096cfbf8c51e1b4b987a01f66f89f8537b6c564
Nadia-Trisha <nadiatrisha68@gmail.com>
2023-09-10 00:13:31 +0600

    jquery

---
+++ Trisha_1276452/JQUERY_METHOD_TEST/index.js
@@ -0,0 +1,29 @@
+
+// Jquery method
+
+// $("#p1").text("hi");
+
+// $("#p1").html("hi");
+
+// $("#p1").append("hi");
+
+// $("#p1").prepend("hi");
+
+// var mypara1 = $("<p></p>").text("This is paragraph 0") 
+// $("#p1").after(mypara1);
+
+// var mypara1 = $("<p></p>").text("This is paragraph 0")
+// $("#p1").after(mypara1);
+
+
+// // Attributes Manipulation
+// $("a").attr("herf");
+
+// $("a").removeAttr("herf");
+
+// $("a").attr("herf", "https://www.studywithans.com"); note: /*set and get attribute ar jonno*/
+
+
+    
+
+
