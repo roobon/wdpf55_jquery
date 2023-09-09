038559ced001806cab3bb25b4c0a92635f6159d1
Roobon <roobon@gmail.com>
2023-09-09 13:17:07 +0600

    Class work 09 Sept 2023

--- jquery/evidence_dropdown/student_list.php
+++ jquery/evidence_dropdown/student_list.php
@@ -1,4 +1,33 @@
 <?php 
+    $server_name= "localhost";
+    $user_name= "root";
+    $password= "";
+    $database_name= "wdpf55_batches";
+    
+    $db= new mysqli($server_name , $user_name , $password , $database_name); 
 
-echo $_POST['id'];
-?>
+    $id = $_POST['id'];
+
+    $result = $db->query("SELECT * FROM students where Batch = '$id'");
+
+    if($result->num_rows>0){
+    echo "<br>";    
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Name</th><th>Gender</th><th>Date of Birth</th><th>Email</th></tr>";
+    while($row = $result->fetch_object()){ ?>
+        <tr>
+            <td><?php echo $row->ID; ?></td>
+            <td><?php echo $row->Name; ?></td>
+            <td><?php echo $row->Gender; ?></td>
+            <td><?php echo $row->DOB; ?></td>
+            <td><?php echo $row->Email; ?></td>
+        </tr> 
+   <?php } }  else {
+        echo "<h3>No data found</h3>";
+   }    
+   ?>
+
+
+    
+
+    
