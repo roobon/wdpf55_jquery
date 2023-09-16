485806ff55ba6d001812253487f850a9d4a6c11a
unknown <kamrulislam.serzy50@gmail.com>
2023-09-16 13:04:11 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/class_evidence/district.php
+++ Ikbal_Hossain_1277699/Class_Practice/class_evidence/district.php
@@ -1,6 +1,8 @@
 <?php 
+    $db = new mysqli("localhost", "root", "", "country");
     $id = $_POST['div_id'];
     echo $id;
 
-    $sql = "Select * FROM district "
+    $sql = "Select * FROM district WHERE dis_id = div_id";
+    $result = $db->query($sql);
 ?>
