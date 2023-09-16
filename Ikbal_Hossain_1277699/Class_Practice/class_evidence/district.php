fd0be88eeab33ca6681c43b9eeb3c5f1d56474d9
ikbalhossainn <ikbalhossain125@gmail.com>
2023-09-16 14:37:47 +0600

    updated

--- Ikbal_Hossain_1277699/Class_Practice/class_evidence/district.php
+++ Ikbal_Hossain_1277699/Class_Practice/class_evidence/district.php
@@ -1,9 +1,21 @@
-<?php 
-    $db = new mysqli("localhost", "root", "", "country");
-    $id = $_POST['a'];
-    // echo $id;
+<?php
+$db = new mysqli("localhost", "root", "", "country");
+$id = $_POST['a'];
+// echo $id;
 
-    $sql = "Select * FROM district WHERE div_id ='$id' ";
-    $result = $db->query($sql);
+$sql = "SELECT * FROM district WHERE div_id ='$id'";
+$result = $db->query($sql);
 ?>
 
+<form action="">
+    <select name="" id="">
+        <option value="">Select One</option>
+
+        <?php while($row=$result->fetch_assoc()){
+        ?>
+
+        <option value="<?php echo $row['dis_id']?>"> <?php echo $row['dis_name']?></option>
+        <?php  }?>
+
+    </select>
+</form>
