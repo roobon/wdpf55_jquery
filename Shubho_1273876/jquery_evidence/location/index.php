41b728efa25c85dcefbb7a0b592871f610fbb27b
mehedishubho <mehedihassanshubho@gmail.com>
2023-09-16 11:01:36 +0600

    16-09-2023

--- Shubho_1273876/jquery_evidence/location/index.php
+++ Shubho_1273876/jquery_evidence/location/index.php
@@ -7,7 +7,7 @@ require_once("config.php");
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta charset="utf-8">
-	<title>PHP GURUKUL | DEMO</title>
+	<title>jQuery | DEMO</title>
 	<meta name="generator" content="Bootply" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
